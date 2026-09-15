fc4f897fe3e6b8cd8492fb3bc7d6bb38827d8d37
payelislam155 <payelislambangladesh100@gmail.com>
2026-09-15 14:04:57 +0600

    register login backend

--- shop/views.py
+++ shop/views.py
@@ -30,7 +30,7 @@ def register_view(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
            user = form.save()
-           login(request, user)
+           login(request, user, backend='django.contrib.auth.backends.ModelBackend')
            messages.success(request,'Registration Successful!')
            return redirect('home')
     else:
