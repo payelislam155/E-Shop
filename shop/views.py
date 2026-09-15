3de42b833895f498357e2e55d460943045fa1576
payelislam155 <payelislambangladesh100@gmail.com>
2026-09-15 13:59:17 +0600

    login backend add

--- shop/views.py
+++ shop/views.py
@@ -18,7 +18,7 @@ def login_view(request):
        user = authenticate(request, username=username,password=password)
 
        if user is not None:
-           login(request, user)
+           login(request, user, backend='django.contrib.auth.backends.ModelBackend')
            messages.success(request,'Login Successful!')
            redirect('home')
        else:
